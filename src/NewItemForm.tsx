ee09f760dde4aa18a26a4a602b28f4688732f084
Akbarjon <akbarkaratayev@gmail.com>
2022-07-05 17:20:24 +0500

    Created state provider for app using context api and hardcoded some test data

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { NewItemButton, NewItemFormContainer, NewItemInput } from './styles';
+import { useFocus } from './utils/useFocus';
 
 interface NewItemFormProps {
   onAdd(text: string): void;
@@ -7,13 +8,16 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('');
+  const inputRef = useFocus();
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
-      <NewItemButton onClick={() => onAdd(text)}>
-        Create
-      </NewItemButton>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
 };
